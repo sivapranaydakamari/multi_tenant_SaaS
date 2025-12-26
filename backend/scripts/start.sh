a6799f64a7093eccaf76a5f1f4f33e0f9149381d
sivapranaydakamari <sivapranay1423@gmail.com>
2025-12-26 23:07:50 +0530

    Added backend scripts

---
+++ backend/scripts/start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+
+echo "⏳ Waiting for database..."
+
+until pg_isready -h "$DB_HOST" -p "$DB_PORT" -U "$DB_USER"; do
+  sleep 2
+done
+
+echo "✅ Database is up"
+
+echo "🚀 Starting backend server"
+node src/server.js
