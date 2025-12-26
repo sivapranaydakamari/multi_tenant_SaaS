0cf5eebbb81c90c6d24ce60f92b6ed672cf6b8ce
sivapranaydakamari <sivapranay1423@gmail.com>
2025-12-26 23:00:19 +0530

    Added execution file of database

---
+++ database/00-run-all.sql
@@ -0,0 +1,7 @@
+\i /docker-entrypoint-initdb.d/migrations/000_init.sql
+\i /docker-entrypoint-initdb.d/migrations/001_create_tenants.sql
+\i /docker-entrypoint-initdb.d/migrations/002_create_users.sql
+\i /docker-entrypoint-initdb.d/migrations/003_create_projects.sql
+\i /docker-entrypoint-initdb.d/migrations/004_create_tasks.sql
+\i /docker-entrypoint-initdb.d/migrations/005_create_audit_logs.sql
+\i /docker-entrypoint-initdb.d/seeds/seed_data.sql
